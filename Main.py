3d5f5ca1fb2ac57556167271eda2bcd6991c516c
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-01-18 18:59:16 -0500

    added a button to Main.py

--- Main.py
+++ Main.py
@@ -5,19 +5,35 @@ from kivy.uix.label import Label
 
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
 class LoginScreen(GridLayout):
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
+        self.inside = GridLayout()
+        self.inside.cols = 2
+        self.cols = 1
+
+        self.inside.add_widget(Label(text='User Name'))
         self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
+        self.inside.add_widget(self.username)
+        self.inside.add_widget(Label(text='password'))
         self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        self.inside.add_widget(self.password)
+        self.add_widget(self.inside)
+
+        self.login = Button(text="Login", font_size=40)
+        self.login.bind(on_pressed=self.login_pressed)
+        self.add_widget(self.login)
+
+    def login_pressed(self, instance):
+        username = self.username.text
+        password = self.password.text
+        print("HIHIHIHIHIHIHIH")
+
+
 
 
 class MyApp(App):
