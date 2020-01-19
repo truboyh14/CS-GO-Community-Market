72e35429cbbaba355fad2e986380c5cd53e5e229
Truman H <trumanhung@gmail.com>
2020-01-19 09:30:13 -0500

    GUI

--- Main.py
+++ Main.py
@@ -10,9 +10,50 @@ from pathlib import Path
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
+from kivy.core.window import Window
 
+Window.clearcolor = (1, 1, 1, 1)
 # kv = Builder.load_file(os.path.join(dirname(__file__), '../uofthacks2020/my.kv'))
 Builder.load_string("""
+<ItemsScreen>:
+    GridLayout:
+        cols: 2
+        GridLayout:
+            rows: 5
+            Label:
+                text: "CS:GO Market Place"
+                color: 0,0,0,1
+                size_hint_x: None
+                width: 200
+            Button:
+                text: "Items"
+                background_color: 1, .3, .4, .85
+                size_hint_x: None
+                width: 200
+                on_press:
+                    items_event()
+            Button:
+                text: "Transactions"
+                size_hint_x: None
+                width: 200
+                on_press:
+                    transactions_event()
+            Button:
+                text: "Users"
+                size_hint_x: None
+                width: 200
+                on_press:
+                    users_event()
+            Button:
+                text: "My vault"                
+                size_hint_x: None
+                width: 200
+                on_press:
+                    vault_event()
+                
+        Label:
+            button: "Right"
+            background_color: 1, .3, .4, .85
 <LoginScreen>:
     GridLayout:
         cols: 1
@@ -32,41 +73,6 @@ Builder.load_string("""
             text:"Login"
             on_press:
                 app.login_event()
-<MainScreen>:
-    GridLayout:
-        cols: 1
-        GridLayout:
-            cols: 2
-            Label:
-                text: "Username"
-            Button:
-                text: "Open Crate"
-                on_press:
-                    app.open_crate()
-        GridLayout:
-            cols: 3
-            Label:
-                text: "Item"
-            Label:
-                text: "Rarity"
-            Label:
-                text: "Owner ID"
-            
-        GridLayout:
-            cols: 3
-            Label:
-                text: "LOL"
-            Label:
-                text: "GG"
-            Button:
-                text: "Trade"
-                on_press:
-                    app.start_trade()
-        Button:
-            text:"Go to my vault"
-            on_press:
-                root.manager.current="second"
-            background_color: [1, 0, 0, 1]
 <LoginFailPopup>
     BoxLayout:
         height: "40dp"
@@ -94,75 +100,101 @@ Builder.load_string("""
 """)
 
 
-class LoginScreen(Screen):
-    pass
-    # def __init__(self, **kwargs):
-    #     super(LoginScreen, self).__init__(**kwargs)
-    #     self.inside = GridLayout()
-    #     self.inside.cols = 2
-    #     self.cols = 1
-    #
-    #     self.inside.add_widget(Label(text='User Name'))
-    #     self.username = TextInput(multiline=False)
-    #     self.inside.add_widget(self.username)
-    #     self.inside.add_widget(Label(text='password'))
-    #     self.password = TextInput(password=True, multiline=False)
-    #     self.inside.add_widget(self.password)
-    #     self.add_widget(self.inside)
-    #
-    #     self.login = Button(text="Login", font_size=40)
-    #     self.add_widget(self.login)
-    #     self.login.bind(on_press=self.login_pressed)
-    #
-    # def login_pressed(self, instance):
-    #     username = self.username.text
-    #     password = self.password.text
-    #     # get data from toda and compare
+# class LoginScreen(Screen):
+#     pass
+#     # def __init__(self, **kwargs):
+#     #     super(LoginScreen, self).__init__(**kwargs)
+#     #     self.inside = GridLayout()
+#     #     self.inside.cols = 2
+#     #     self.cols = 1
+#     #
+#     #     self.inside.add_widget(Label(text='User Name'))
+#     #     self.username = TextInput(multiline=False)
+#     #     self.inside.add_widget(self.username)
+#     #     self.inside.add_widget(Label(text='password'))
+#     #     self.password = TextInput(password=True, multiline=False)
+#     #     self.inside.add_widget(self.password)
+#     #     self.add_widget(self.inside)
+#     #
+#     #     self.login = Button(text="Login", font_size=40)
+#     #     self.add_widget(self.login)
+#     #     self.login.bind(on_press=self.login_pressed)
+#     #
+#     # def login_pressed(self, instance):
+#     #     username = self.username.text
+#     #     password = self.password.text
+#     #     # get data from toda and compare
 
 
-class MainScreen(Screen):
-    pass
-
-
-class LoginFailPopup(Screen):
+class ItemsScreen(Screen):
     pass
 
 
-class CreateNewAccountPopup(Screen):
+class TransactionsScreen(Screen):
     pass
+
+
+class UsersScreen(Screen):
+    pass
+
+
+class VaultScreen(Screen):
+    pass
+
+
+# class LoginFailPopup(Screen):
+#     pass
+#
+#
+# class CreateNewAccountPopup(Screen):
+#     pass
 
 
 wm = ScreenManager()
-wm.add_widget(MainScreen(name='main'))
-wm.add_widget(LoginScreen(name='second'))
-wm.add_widget(LoginFailPopup(name='fail_login'))
+wm.add_widget(ItemsScreen(name='items'))
+wm.add_widget(TransactionsScreen(name='transactions'))
+wm.add_widget(UsersScreen(name='users'))
+wm.add_widget(VaultScreen(name='vault'))
+
+
+# wm.add_widget(LoginScreen(name='second'))
+# wm.add_widget(LoginFailPopup(name='fail_login'))
 
 
 class Main(App):
     def build(self):
         return wm
 
-    def login_event(self):
-        # CHECK LOGIN CREDENTIAL
-        # if PASS
-        self.root.current = "main"
-        # if FAIL
-        self.show_pop_up()
+    # def login_event(self):
+    #     # CHECK LOGIN CREDENTIAL
+    #     # if PASS
+    #     self.root.current = "items"
+    #     # if FAIL
+    #     self.show_pop_up()
 
-    def show_pop_up(self):
-        show = LoginFailPopup()
-        popupWindow = Popup(title="Login", content=show,
-                            size_hint=(None, None), size=(400, 400))
-        popupWindow.open()
+    def transactions_event(self):
+        self.root.current = "transactions"
 
-    def create_new_account(self):
-        pass
+    def users_event(self):
+        self.root.current = "users"
 
-    def open_crate(self):
-        pass
+    def vault_event(self):
+        self.root.current = "vault"
 
-    def start_trade(self):
-        pass
+    # def show_pop_up(self):
+    #     show = LoginFailPopup()
+    #     popupWindow = Popup(title="Login", content=show,
+    #                         size_hint=(None, None), size=(400, 400))
+    #     popupWindow.open()
+
+    # def create_new_account(self):
+    #     pass
+    #
+    # def open_crate(self):
+    #     pass
+    #
+    # def start_trade(self):
+    #     pass
 
 
 if __name__ == '__main__':
